fbb394935fd994e4e82946280514b6ee90053a3c
ssarosiek99 <sarosiekszymon@gmail.com>
2023-06-09 11:25:03 +0200

    Added flask server

--- consumer.py
+++ consumer.py
@@ -5,8 +5,10 @@ import matplotlib.pyplot as plt
 import sklearn
 import pandas as pd
 import lightgbm as ltb
+import random
+from flask import Flask
 
-# Getting the data as JSON
+# # Getting the data as JSON
 consumer = KafkaConsumer('data-stream',
 bootstrap_servers=['localhost:9092'],
 value_deserializer=lambda m: json.loads(m.decode('ascii')))
@@ -38,9 +40,27 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
 model = ltb.LGBMRegressor()
 model.fit(X_train, y_train)
 
+app = Flask(__name__)
 
-for message in consumer:
+@app.route('/get-prediction/')
+def home():
+    for message in consumer:
 
-    df2 = pd.read_json(message.value)
-    predicted_y = model.predict(df2.values[:1])
-    print( predicted_y)
+        df2 = pd.read_json(message.value)
+        ran = random.randint(1, 50)
+        predicted_y = model.predict(df2.values[:ran])
+        print(df2.values[:ran][0][1])
+
+        if predicted_y[0] > 20:
+             decision = " Nie wychodz z domu /n"
+        else:
+             decision = " Mozna wyjsc z domu"
+
+        return "Na podstawie modelu promieniowanie sloneczne krtokofalowe wyniesie " + str(predicted_y[0]) + " \n" + decision + "\n Wyboru dokonano na podstawie danych temperature_2m_max {}, temperature_2m_min {}, temperature_2m_mean {}, rain_sum {}, windspeed_10m_max {}, windgusts_10m_max  {}, et0_fao_evapotranspiration {}   ".format(df2.values[:ran][0][0],df2.values[:ran][0][1], df2.values[:ran][0][2] ,df2.values[:ran][0][3] ,df2.values[:ran][0][4],df2.values[:ran][0][5],df2.values[:ran][0][6]) 
+
+
+
+
+if __name__ == "__main__":
+    app.run()
+
