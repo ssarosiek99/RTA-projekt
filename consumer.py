77a98915acd4a0d8829b230d13be8cb149e189c0
marcin73 <134865862+marcin73@users.noreply.github.com>
2023-06-10 11:46:14 +0200

    Add files via upload
    prawie gotowe

--- consumer.py
+++ consumer.py
@@ -6,7 +6,7 @@ import sklearn
 import pandas as pd
 import lightgbm as ltb
 import random
-from flask import Flask
+from flask import Flask, render_template, request
 
 # # Getting the data as JSON
 consumer = KafkaConsumer('data-stream',
@@ -14,51 +14,30 @@ bootstrap_servers=['localhost:9092'],
 value_deserializer=lambda m: json.loads(m.decode('ascii')))
 
 
-df = pd.read_json("https://archive-api.open-meteo.com/v1/archive?latitude=52.52&longitude=13.41&start_date=2019-05-17&end_date=2023-05-31&daily=temperature_2m_max,temperature_2m_min,temperature_2m_mean,sunrise,sunset,shortwave_radiation_sum,rain_sum,windspeed_10m_max,windgusts_10m_max,et0_fao_evapotranspiration&timezone=auto")
-
-df = pd.DataFrame(df['daily'].tolist())
-
-df = df.T
-
-df = df.rename(columns = {0 : "Data", 1 : "temperature_2m_max", 2: "temperature_2m_min", 3 : "temperature_2m_mean", 4 : "sunrise", 5 : "sunset", 6 : "shortwave_radiation_sum", 7 : "rain_sum", 8 : "windspeed_10m_max", 9 : "windgusts_10m_max", 10 : "et0_fao_evapotranspiration"})
-
-    ############### usuwam ostanie dwie obserwacje, jakieś nan tam są
-df = df[0:len(df)-2]
-
-X = df.drop(columns = ['shortwave_radiation_sum', 'Data', 'sunrise', 'sunset'])
-y = df.shortwave_radiation_sum
-
-for col in X.columns:
-        X[col] = X[col].astype('float')
-
-X = X.astype(float)
-y = y.astype(float)
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
-
-
-model = ltb.LGBMRegressor()
-model.fit(X_train, y_train)
-
 app = Flask(__name__)
+
+
 
 @app.route('/get-prediction/')
 def home():
     for message in consumer:
+        print("%s key=%s value=%s" % (message.topic, message.key, message.value))
+        global lat
+        lat = message.value[1]
+        global len
+        len = message.value[2]
+        return(str((message.value[0])))
 
-        df2 = pd.read_json(message.value)
-        ran = random.randint(1, 50)
-        predicted_y = model.predict(df2.values[:ran])
-        print(df2.values[:ran][0][1])
-
-        if predicted_y[0] > 20:
-             decision = " Nie wychodz z domu /n"
-        else:
-             decision = " Mozna wyjsc z domu"
-
-        return "Na podstawie modelu promieniowanie sloneczne krtokofalowe wyniesie " + str(predicted_y[0]) + " \n" + decision + "\n Wyboru dokonano na podstawie danych temperature_2m_max {}, temperature_2m_min {}, temperature_2m_mean {}, rain_sum {}, windspeed_10m_max {}, windgusts_10m_max  {}, et0_fao_evapotranspiration {}   ".format(df2.values[:ran][0][0],df2.values[:ran][0][1], df2.values[:ran][0][2] ,df2.values[:ran][0][3] ,df2.values[:ran][0][4],df2.values[:ran][0][5],df2.values[:ran][0][6]) 
-
-
+@app.route('/get-prediction2/')
+def root():
+    markers=[
+        {
+        'lat': len,
+        'lon': lat,
+        'popup':'Jestesmy w'  
+        }
+    ]
+    return render_template('index.html',markers=markers )
 
 
 if __name__ == "__main__":
--- producer.py
+++ producer.py
@@ -3,6 +3,10 @@ from time import sleep
 import requests
 import pandas as pd
 import logging
+import random
+from sklearn.model_selection import train_test_split
+import lightgbm as ltb
+
 
 import json
 
@@ -24,13 +28,61 @@ y = df.shortwave_radiation_sum
 
 # Producing as JSON
 producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
+api_version=(0,11,5),
 value_serializer=lambda m: json.dumps(m).encode('ascii'))
+
+i = 1
 
 while(True):
     sleep(2)
+
+    lat = random.randint(1,90)
+    len = random.randint(1,180)
+
+    print(lat,len)
+
+    url = "https://archive-api.open-meteo.com/v1/archive?latitude=" + str(lat) + "&longitude=" + str(len) + "&start_date=2019-05-17&end_date=2023-05-31&daily=temperature_2m_max,temperature_2m_min,temperature_2m_mean,sunrise,sunset,shortwave_radiation_sum,rain_sum,windspeed_10m_max,windgusts_10m_max,et0_fao_evapotranspiration&timezone=auto"
+
+
+    df = pd.read_json(url)
+
+    df = pd.DataFrame(df['daily'].tolist())
+
+    df = df.T
+
+    df = df.rename(columns = {0 : "Data", 1 : "temperature_2m_max", 2: "temperature_2m_min", 3 : "temperature_2m_mean", 4 : "sunrise", 5 : "sunset", 6 : "shortwave_radiation_sum", 7 : "rain_sum", 8 : "windspeed_10m_max", 9 : "windgusts_10m_max", 10 : "et0_fao_evapotranspiration"})
+
     
+    ############### usuwam ostanie dwie obserwacje, jakieś nan tam są
+    df = df.dropna()
+
+    X = df.drop(columns = ['shortwave_radiation_sum', 'Data', 'sunrise', 'sunset'])
+    y = df.shortwave_radiation_sum
+
+    for col in X.columns:
+        X[col] = X[col].astype('float')
+    X = X.astype(float)
+    y = y.astype(float)
+
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
+
+    model = ltb.LGBMRegressor()
+    model.fit(X_train, y_train)
+    
+    pred = model.predict(X.tail(1))
+
+    array  = []
+
+    array.append(pred[0])
+    array.append(len)
+    array.append(lat)
+
     print("Price fetched")
-    producer.send('data-stream', X.to_json()).get(timeout=100)
+
+    producer.send('data-stream', array)
     producer.flush(timeout = 100)
 
-    print("Price sent to consumer")
+    i = i + 1    
+
+    print("sent")
